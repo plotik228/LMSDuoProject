42feb7529cedee5fe4ac632a82bc8d28f4eb9a89
vikakirieshkina <68634728+Yakushac@users.noreply.github.com>
2025-01-19 21:19:48 +0400

    Update ggg.py

--- ggg.py
+++ ggg.py
@@ -12,6 +12,7 @@ def terminate():
     pygame.quit()
     sys.exit()
 
+
 def draw(screen, width, height):
     for i in range(2222):
         screen.fill(pygame.Color((160, 160, 160)),
@@ -69,6 +70,7 @@ if __name__ == '__main__':
 clock = pygame.time.Clock()
 start_time = time.time()
 
+
 def rules_render(screen, rule, y, cnt):
     if cnt == 0:
         font = pygame.font.SysFont('Consolas', 20)
@@ -90,35 +92,41 @@ def rules_render(screen, rule, y, cnt):
     else:
         return
 
+
 rules_1 = ["Алло, алло?", "",
-                   "Эм, я решил записать для тебя сообщение, чтобы помочь тебе освоиться в первую ночь. Вообще, я работал в этом офисе ещё до тебя.",
-                   "Я как раз заканчиваю свою последнюю неделю. Я знаю, что ты немного взволнован, но беспокоиться вовсе не о чем, всё будет отлично. ",
-                   "Так что давай сосредоточимся на том, что я помогу тебе пройти первый день. Договорились?"]
+           "Эм, я решил записать для тебя сообщение, чтобы помочь тебе освоиться в первую ночь. Вообще, я работал в этом офисе ещё до тебя.",
+           "Я как раз заканчиваю свою последнюю неделю. Я знаю, что ты немного взволнован, но беспокоиться вовсе не о чем, всё будет отлично. ",
+           "Так что давай сосредоточимся на том, что я помогу тебе пройти первый день. Договорились?"]
 
-rules_2 = ["Ну, сначала я должен прочитать вступительное приветствие от компании. Юридическая ерунда, сам знаешь: «Добро пожаловать в «Freddy Fazbear's Pizza». ",
-                   "Волшебное место для детей и взрослых, где оживают фантазии и веселье. Компания Fazbear Entertainment не несет ответственности ",
-                   "за ущерб имуществу или людям. ",
-                   "В случае обнаружения ущерба или смерти, заявление о пропаже подаётся в течение 90 дней, или после тщательной очистки имущества ",
-                   "и всех помещений, и замены ковровых покрытий.»"]
+rules_2 = [
+    "Ну, сначала я должен прочитать вступительное приветствие от компании. Юридическая ерунда, сам знаешь: «Добро пожаловать в «Freddy Fazbear's Pizza». ",
+    "Волшебное место для детей и взрослых, где оживают фантазии и веселье. Компания Fazbear Entertainment не несет ответственности ",
+    "за ущерб имуществу или людям. ",
+    "В случае обнаружения ущерба или смерти, заявление о пропаже подаётся в течение 90 дней, или после тщательной очистки имущества ",
+    "и всех помещений, и замены ковровых покрытий.»"]
 
-rules_3 = ["Бла-бла-бла, звучит паршиво, я знаю, но волноваться не о чем. Ну, здешние аниматроники немного странно себя ведут по ночам, но разве я их виню? Нет. ",
-           "Если бы меня заставляли петь одни и те же дурацкие песенки в течение двадцати лет и не давали бы сходить в душ, то я был бы тоже раздражителен. ",
-           "Так что, помни, эти персонажи занимают особое место в сердцах детей, и нужно проявить к ним немного уважения, верно? Хорошо."]
+rules_3 = [
+    "Бла-бла-бла, звучит паршиво, я знаю, но волноваться не о чем. Ну, здешние аниматроники немного странно себя ведут по ночам, но разве я их виню? Нет. ",
+    "Если бы меня заставляли петь одни и те же дурацкие песенки в течение двадцати лет и не давали бы сходить в душ, то я был бы тоже раздражителен. ",
+    "Так что, помни, эти персонажи занимают особое место в сердцах детей, и нужно проявить к ним немного уважения, верно? Хорошо."]
 
-rules_4 = ["Так что, просто знай, эти персонажи могут ходить сами по себе. Их оставляют в каком-то «режиме свободного передвижения» по ночам. ",
-           "Вроде из-за того, что их сервоприводы блокируются, если они будут выключены слишком долго. Раньше им разрешалось ходить и днем. ",
-           "Но в 87-м произошёл инцидент с укусом. Поразительно, что человеческое тело может жить без лобной доли, да?"]
+rules_4 = [
+    "Так что, просто знай, эти персонажи могут ходить сами по себе. Их оставляют в каком-то «режиме свободного передвижения» по ночам. ",
+    "Вроде из-за того, что их сервоприводы блокируются, если они будут выключены слишком долго. Раньше им разрешалось ходить и днем. ",
+    "Но в 87-м произошёл инцидент с укусом. Поразительно, что человеческое тело может жить без лобной доли, да?"]
 
-rules_5 = ["Эм, касательно безопасности - единственный реальный риск для тебя как ночного охранника здесь, если он вообще есть, это то, что эти персонажи, ",
-           "эм, если они увидят тебя в нерабочее время, то они, вероятно, не распознают в тебе человека. ",
-           "Они, скорее всего, увидят в тебе металлический эндоскелет без костюма. Так как это противоречит правилам в пиццерии Фредди Фазбера, ",
-           "они, вероятно, попытаются... насильно затолкать тебя в костюм Фредди Фазбера. Вроде и нет в этом ничего страшного, если б сами костюмы",
-           "не были заполнены траверсами, проводами и ",
-           "прочими аниматронными механизмами, особенно вокруг лицевой части. Так что, попробуй представить, как твою голову с силой вдавливают туда, ",
-           "и это причиняет тебе небольшой дискомфорт... и смерть. Белый свет увидят лишь твои глаза и зубы, которые выскочат из передней части маски, хех."]
+rules_5 = [
+    "Эм, касательно безопасности - единственный реальный риск для тебя как ночного охранника здесь, если он вообще есть, это то, что эти персонажи, ",
+    "эм, если они увидят тебя в нерабочее время, то они, вероятно, не распознают в тебе человека. ",
+    "Они, скорее всего, увидят в тебе металлический эндоскелет без костюма. Так как это противоречит правилам в пиццерии Фредди Фазбера, ",
+    "они, вероятно, попытаются... насильно затолкать тебя в костюм Фредди Фазбера. Вроде и нет в этом ничего страшного, если б сами костюмы",
+    "не были заполнены траверсами, проводами и ",
+    "прочими аниматронными механизмами, особенно вокруг лицевой части. Так что, попробуй представить, как твою голову с силой вдавливают туда, ",
+    "и это причиняет тебе небольшой дискомфорт... и смерть. Белый свет увидят лишь твои глаза и зубы, которые выскочат из передней части маски, хех."]
 
-rules_6 = ["Да-да, они не сказали тебе об этом при регистрации. Но, эй, первый день - легче лёгкого. Я поговорю с тобой завтра. ",
-           "Проверяй камеры и закрывай двери только в случае крайней необходимости. Нужно экономить энергию. Ладно, спокойной ночи."]
+rules_6 = [
+    "Да-да, они не сказали тебе об этом при регистрации. Но, эй, первый день - легче лёгкого. Я поговорю с тобой завтра. ",
+    "Проверяй камеры и закрывай двери только в случае крайней необходимости. Нужно экономить энергию. Ладно, спокойной ночи."]
 cnt1 = 0
 cnt2 = 0
 cnt3 = 0
@@ -176,6 +184,7 @@ if __name__ == '__main__':
 
     pygame.quit()
 
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('fnaf', name)
     if not os.path.isfile(fullname):
@@ -184,20 +193,23 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
-def fnaf_button_monitor(screen):
-    button = load_image("fnaf_button_monitor.png")
-    screen.blit(button, (335, 650))
 
-def security(screen):
-    sec = load_image("sec1.png")
-    screen.blit(sec, (0, 0))
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+pygame.init()
+width, height = 1600, 720
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('FNAF1')
 
 
 class Vents(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.images = []
-        for i in range(1 ,4):
+        for i in range(1, 4):
             image = load_image(f"vent{i}.png")
             self.images.append(image)
         self.cur_frame = 0
@@ -229,7 +241,6 @@ class MonitorUp(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=(x, y))
         self.last_frame_time = 0
         self.frame_delay = 10
-        self.animated_finish = False
 
     def update(self, animated_playing, animated_finished):
         if animated_playing:
@@ -243,21 +254,50 @@ class MonitorUp(pygame.sprite.Sprite):
                     animated_playing = False
                     animated_finished = True
 
+    def gamestate(self):
+        if self.cur_frame == len(self.images):
+            return 'cameras'
+        else:
+            return 'office'
 
-def buttondown():
-    if (pygame.mouse.get_pos()[0] >= 335 and pygame.mouse.get_pos()[0] <= 935
-            and pygame.mouse.get_pos()[1] >= 650 and pygame.mouse.get_pos()[1] <= 710):
-       return True
+
+class CameraUp(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.images = []
+        for i in range(1, 13):
+            image = load_image(f"camera{i}.png")
+            image = pygame.transform.scale(image, (1600, 720))
+            self.images.append(image)
+        self.cur_frame = 0
+        self.image = self.images[self.cur_frame]
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.last_frame_time = 0
+        self.frame_delay = 10
+
+    def update(self, ap2, af2):
+        if ap2:
+            cur_time = pygame.time.get_ticks()
+            if cur_time - self.last_frame_time >= self.frame_delay:
+                self.cur_frame = self.cur_frame + 1
+                if self.cur_frame < len(self.images):
+                    self.image = self.images[self.cur_frame]
+                    self.last_frame_time = cur_time
+                else:
+                    ap2 = False
+                    af2 = True
+                    screen.blit(load_image("a1default.png"), (0, 0))
+
+
+def button():
+    if pygame.mouse.get_pos()[0] >= 335 and pygame.mouse.get_pos()[0] <= 935 \
+            and pygame.mouse.get_pos()[1] >= 650 and pygame.mouse.get_pos()[1] <= 710:
+        return True
     else:
         return False
 
 
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 1600, 720
-    screen = pygame.display.set_mode(size)
-    screen.fill((0, 0, 0))
-
+def office():
     all_vents = pygame.sprite.GroupSingle()
     vents = Vents(780, 303)
     all_vents.add(vents)
@@ -266,31 +306,48 @@ if __name__ == '__main__':
     monitors = MonitorUp(0, 0)
     all_mon_up.add(monitors)
 
+    all_camerasup = pygame.sprite.GroupSingle()
+    camerasup = CameraUp(0, 0)
+    all_camerasup.add(camerasup)
+
+    ap2 = True
+    af2 = False
+
     animated_finished = False
     animated_playing = False
-    clock = pygame.time.Clock()
 
+    gamestate = 'office'
     running = True
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if not animated_finished and buttondown():  # Запускаем анимацию, только если она не завершена
-                    animated_playing = True
-                    last_frame_time = pygame.time.get_ticks()
+        if gamestate == 'office':
+            screen.blit(load_image("sec1.png"), (0, 0))
 
-        security(screen)
-        fnaf_button_monitor(screen)
+            all_vents.update()
+            all_vents.draw(screen)
 
-        all_vents.update()
-        all_vents.draw(screen)
+            screen.blit(load_image("fnaf_button_monitor.png"), (335, 650))
 
-        all_mon_up.draw(screen)
-        all_mon_up.update(animated_playing, animated_finished)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if button() and animated_finished == False:
+                        animated_playing = True
+
+            all_mon_up.draw(screen)
+            all_mon_up.update(animated_playing, animated_finished)
+
+            gamestate = monitors.gamestate()
+
+        elif gamestate == 'cameras':
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+            screen.fill((0, 0, 0))
+
+            all_camerasup.draw(screen)
+            all_camerasup.update(ap2, af2)
 
         pygame.display.flip()
 
-    pygame.quit()
-
-#Я НЕ ЗНАЮ КАК СДЕЛАТЬ ЭТО ВОНЮЧЕЕ СВОРАЧИВАНИЕ МОНИТОРА И ВОНЮЧЕЕ ВСЕ И ВОНЮЧИЕ КАМЕРЫ
+office()
